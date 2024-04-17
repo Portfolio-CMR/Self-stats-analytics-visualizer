69bf45c3377241a50a68000e7b7ea8dcedcf6be7
coltonrobbins73 <coltonrobbins73@gmail.com>
2024-04-17 10:21:30 -0700

    added scrollbar and day of the week plot

--- self_stats/dash_app.py
+++ self_stats/dash_app.py
@@ -12,6 +12,7 @@ app = Dash(__name__)
 app.layout = html.Div([
     html.H1("Entries Frequency Over Time", style={'textAlign': 'center', 'color': 'white'}),
     dcc.Graph(id='time-series-chart', config={'staticPlot': False}),  # Allow zoom and pan
+    dcc.Graph(id='weekday-chart', config={'staticPlot': False}),  # New graph for day of the week frequency
     dcc.Interval(
         id='interval-component',
         interval=1*60000,  # in milliseconds
@@ -19,15 +20,12 @@ app.layout = html.Div([
     ),
     dcc.Store(id='stored-data'),  # To store the initial full dataset
 ], className='row', style={
-    'verticalAlign': 'middle',
     'textAlign': 'center',
-    'position': 'fixed',
     'width': '100%',
-    'height': '100%',
-    'top': '0px',
-    'left': '0px',
-    'zIndex': '1000'
+    'maxHeight': '100vh',
+    'overflowY': 'auto'  # Enable vertical scroll
 })
+################################################################################
 
 @app.callback(
     Output('stored-data', 'data'),
@@ -62,6 +60,31 @@ def load_data(n):
     df = df[df['Date'] >= changepoint_date]
 
     return df.to_json(date_format='iso', orient='split')
+
+@app.callback(
+    Output('weekday-chart', 'figure'),
+    [Input('stored-data', 'data')]
+)
+
+def update_weekday_graph(json_data):
+    df = pd.read_json(json_data, orient='split')
+    df['Date'] = pd.to_datetime(df['Date'])
+    df['Weekday'] = df['Date'].dt.day_name()
+
+    # Count the number of entries per weekday and sort them by the order of the week
+    weekday_counts = df.groupby('Weekday').size().reindex(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'])
+
+    fig = px.bar(weekday_counts, x=weekday_counts.index, y=weekday_counts, title="Frequency of Entries by Day of the Week")
+    fig.update_traces(marker_color='teal')
+    fig.update_layout(
+        plot_bgcolor='rgba(0,0,0,0)',
+        paper_bgcolor='rgba(0,0,0,0)',
+        font_color="white"
+    )
+
+    return fig
+
+################################################################################
 
 # Assuming that 'num_bins' is the desired number of bins when fully zoomed out
 # You can adjust 'num_bins' based on your dataset and preference

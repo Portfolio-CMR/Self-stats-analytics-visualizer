a07b296618455b76a7527f08654ee65cc03aa625
coltonrobbins73 <coltonrobbins73@gmail.com>
2024-04-17 11:52:16 -0700

    Added hour of day functionality and linked all plots

--- self_stats/dash_app.py
+++ self_stats/dash_app.py
@@ -4,6 +4,7 @@ import ruptures as rpt
 import plotly.express as px
 from dash import Dash, html, dcc
 from dash.dependencies import Input, Output
+from io import StringIO
 
 CSV_FILE_PATH = '/home/bio/Python_projects/self_stats/data/extracted_watch_history_data.csv'
 
@@ -13,6 +14,7 @@ app.layout = html.Div([
     html.H1("Entries Frequency Over Time", style={'textAlign': 'center', 'color': 'white'}),
     dcc.Graph(id='time-series-chart', config={'staticPlot': False}),  # Allow zoom and pan
     dcc.Graph(id='weekday-chart', config={'staticPlot': False}),  # New graph for day of the week frequency
+    dcc.Graph(id='hour-chart', config={'staticPlot': False}),  # New graph for time of day frequency
     dcc.Interval(
         id='interval-component',
         interval=1*60000,  # in milliseconds
@@ -23,22 +25,27 @@ app.layout = html.Div([
     'textAlign': 'center',
     'width': '100%',
     'maxHeight': '100vh',
-    'overflowY': 'auto'  # Enable vertical scroll
+    'overflowY': 'auto'  # Enable verticasl scroll
 })
+
 ################################################################################
 
 @app.callback(
     Output('stored-data', 'data'),
     [Input('interval-component', 'n_intervals')]
 )
-
 def load_data(n):
     df = pd.read_csv(CSV_FILE_PATH)
     df['Date'] = pd.to_datetime(df['Date'], errors='coerce')
     df = df.dropna(subset=['Date'])
 
-    # Bin data by day and count the number of entries per day
+    # Directly extract the hour from the 'Date' column
+    df['Hour'] = df['Date'].dt.hour
+
+    # Now convert 'Date' to just the date part
     df['Date'] = df['Date'].dt.date
+
+    # Bin data by day and count the number of entries per day
     daily_counts = df.groupby('Date').size()
 
     # Convert the daily counts to a 2D array for ruptures
@@ -59,23 +66,31 @@ def load_data(n):
     # Filter the DataFrame to only include data from after the changepoint
     df = df[df['Date'] >= changepoint_date]
 
+    # Return data as JSON, include both 'Date' and 'Time' columns
     return df.to_json(date_format='iso', orient='split')
+
+################################################################################
 
 @app.callback(
     Output('weekday-chart', 'figure'),
-    [Input('stored-data', 'data')]
+    [Input('stored-data', 'data'),
+     Input('time-series-chart', 'relayoutData')]
 )
-
-def update_weekday_graph(json_data):
-    df = pd.read_json(json_data, orient='split')
+def update_weekday_graph(json_data, relayoutData):
+    df = pd.read_json(StringIO(json_data), orient='split')
     df['Date'] = pd.to_datetime(df['Date'])
     df['Weekday'] = df['Date'].dt.day_name()
+
+    # Check if there is zoom range info in relayoutData and adjust the DataFrame accordingly
+    if relayoutData and 'xaxis.range[0]' in relayoutData and 'xaxis.range[1]' in relayoutData:
+        x_start, x_end = pd.to_datetime(relayoutData['xaxis.range[0]']), pd.to_datetime(relayoutData['xaxis.range[1]'])
+        df = df[(df['Date'] >= x_start) & (df['Date'] <= x_end)]
 
     # Count the number of entries per weekday and sort them by the order of the week
     weekday_counts = df.groupby('Weekday').size().reindex(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'])
 
     fig = px.bar(weekday_counts, x=weekday_counts.index, y=weekday_counts, title="Frequency of Entries by Day of the Week")
-    fig.update_traces(marker_color='teal')
+    fig.update_traces(marker_color='green')
     fig.update_layout(
         plot_bgcolor='rgba(0,0,0,0)',
         paper_bgcolor='rgba(0,0,0,0)',
@@ -83,6 +98,39 @@ def update_weekday_graph(json_data):
     )
 
     return fig
+
+################################################################################
+
+@app.callback(
+    Output('hour-chart', 'figure'),
+    [Input('stored-data', 'data'),
+     Input('time-series-chart', 'relayoutData')]
+)
+def update_hour_graph(json_data, relayoutData):
+    df = pd.read_json(StringIO(json_data), orient='split')
+    # df['Time'] = pd.to_datetime(df['Time'])
+    # df['Hour'] = df['Time'].dt.hour  # Extract hour from the Date
+
+    # Check if there is zoom range info in relayoutData and adjust the DataFrame accordingly
+    if relayoutData and 'xaxis.range[0]' in relayoutData and 'xaxis.range[1]' in relayoutData:
+        x_start, x_end = pd.to_datetime(relayoutData['xaxis.range[0]']), pd.to_datetime(relayoutData['xaxis.range[1]'])
+        df = df[(df['Date'] >= x_start) & (df['Date'] <= x_end)]
+
+    # Count the number of entries per hour
+    hour_counts = df.groupby('Hour').size()
+
+    fig = px.bar(hour_counts, x=hour_counts.index, y=hour_counts, title="Frequency of Entries by Time of Day")
+    fig.update_traces(marker_color='darkblue')
+    fig.update_layout(
+        plot_bgcolor='rgba(0,0,0,0)',
+        paper_bgcolor='rgba(0,0,0,0)',
+        font_color="white",
+        xaxis=dict(tickmode='array', tickvals=list(range(24)), title='Hour of the Day'),
+        yaxis_title="Number of Entries"
+    )
+
+    return fig
+
 
 ################################################################################
 
@@ -96,7 +144,7 @@ num_bins = 20
      Input('time-series-chart', 'relayoutData')]
 )
 def update_graph(json_data, relayoutData):
-    df = pd.read_json(json_data, orient='split')
+    df = pd.read_json(StringIO(json_data), orient='split')
 
     # Check if there is zoom range info in relayoutData
     if relayoutData and 'xaxis.range[0]' in relayoutData and 'xaxis.range[1]' in relayoutData:

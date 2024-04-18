f83b3e4976a6633c0adca57046961ec0329b8ae7
coltonrobbins73 <coltonrobbins73@gmail.com>
2024-04-18 08:50:50 -0700

    added commentable argumetns for easier testing teting

--- self_stats/__main__.py
+++ self_stats/__main__.py
@@ -1,35 +1,46 @@
 from self_stats.munger.selector import get_file_presence_flags
 from self_stats.munger.munger_main import main as munger_main
+from self_stats.dash_app.dash_app_caller import main as dash_main
 
 def main() -> None:
     """
     Main function that orchestr
     ates the processing of watch history and search history based on file presence.
     """
-    directory: str = input("Enter the directory path where your input data is held: ")
-    print(f"\Initializing from directory: {directory}...\n")
+    skip_preprocess = False
+    # directory = 'data'
 
-    file_flags: dict = get_file_presence_flags(directory)
+    if not skip_preprocess:
+        directory: str = input("Enter the directory path where your input data is held: ")
+        print(f"\Initializing from directory: {directory}...\n")
 
-    if file_flags['my_activity_present']:
-        print("Processing search history...\n")
-        munger_main(directory, f'{directory}/MyActivity.json', [
-            'Text Title',
-            'Date',
-            'Latitude',
-            'Longitude'
-        ])
+        file_flags: dict = get_file_presence_flags(directory)
 
-    if file_flags['watch_history_present']:
-        print("Processing watch history...\n")
-        munger_main(directory, f'{directory}/watch-history.json', [
-            'Video URL',
-            'Video Title',
-            'Channel Title',
-            'Date'
-        ])
+        if file_flags['my_activity_present']:
+            print("Processing search history...\n")
+            munger_main(directory, f'{directory}/MyActivity.json', [
+                'Text Title',
+                'Date',
+                'Latitude',
+                'Longitude'
+            ])
+
+        if file_flags['watch_history_present']:
+            print("Processing watch history...\n")
+            munger_main(directory, f'{directory}/watch-history.json', [
+                'Video URL',
+                'Video Title',
+                'Channel Title',
+                'Date'
+            ])
     
     #TODO Implement dash visualization sub-module here
+
+    # viz_flag = 'search'
+
+    # if viz_flag == 'watch':
+    #     print("Visualizing watch history...\n")
+    #     dash_main('data/output/dash_ready_watch_data.csv')
 
 if __name__ == "__main__":
     main()

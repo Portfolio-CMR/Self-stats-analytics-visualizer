8f76eaf93977d092ad76879288969f4253efd5a3
coltonrobbins73 <coltonrobbins73@gmail.com>
2024-04-26 14:58:42 -0700

    consolidated functions

--- self_stats/munger/input_output.py
+++ self_stats/munger/input_output.py
@@ -4,7 +4,6 @@ import numpy as np
 from pathlib import Path
 from typing import Any, Dict, List, Optional, Tuple
 
-
 def read_json_file(file_path: str) -> List[Dict[str, Any]]:
     """
     Loads JSON data from a specified file.
@@ -38,3 +37,31 @@ def save_to_csv(data: Tuple[np.ndarray, ...], filepath: str | Path, mappings: Li
         # Write the rows to the CSV file
         writer.writerows(combined_data)
 
+def ensure_directory_exists(directory: str) -> None:
+    """Ensure that the specified directory exists.
+    
+    Args:
+        directory (str): The path to the directory to check.
+
+    Raises:
+        ValueError: If the directory does not exist.
+    """
+    path = Path(directory)
+    if not path.is_dir():
+        raise ValueError(f"Directory {directory} does not exist. Please ensure it is created and accessible.")
+
+def get_file_presence_flags(directory: str) -> Dict[str, bool]:
+    """Check for the presence of specific files in a given directory and return their presence as flags.
+    
+    Args:
+        directory (str): The directory in which to check for files.
+
+    Returns:
+        Dict[str, bool]: A dictionary with boolean flags for each file type detected.
+    """
+    ensure_directory_exists(directory)
+    path = Path(directory)
+    return {
+        'watch_history_present': (path / 'watch-history.json').exists(),
+        'my_activity_present': (path / 'MyActivity.json').exists()
+    }
